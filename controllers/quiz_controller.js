a158488b76bffc035e608d54b7a8de37f500c427
Yersyn S M <yersyn@gmail.com>
2015-07-13 15:14:48 -0500

    primera parte
    primer commit, todo lo que s  hizo en clase

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+//GET quizez question
+
+exports.question=function (req,res) {
+	res.render('quizes/question',{pregunta:'capital de italia'});
+};
+
+exports.answer=function(req,res){
+	if (req.query.respuesta==='Roma') {
+		res.render('quizes/answer',{respuesta:'Correcto'});
+	}else{
+		res.render('quizes/answer',{respuesta:'Incorrecto'});
+	};
+};
